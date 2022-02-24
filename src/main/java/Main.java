a9ecda5827ea13e63b5dd6251fa4da787f6ab5f8
mKazmierczak1 <m.kazmierczak512@gmail.com>
2022-02-24 21:06:51 +0100

    Overload printMostProbableResults method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 
 public class Main {
 
@@ -11,7 +12,7 @@ public class Main {
 
     public static void main(String[] args) {
         final String filePath = "./src/main/resources/data.json";
-
+        Scanner scanner = new Scanner(System.in);
         EventsData eventsData = new EventsData();
 
         try {
@@ -25,12 +26,24 @@ public class Main {
 
         Collections.sort(eventsList);               // sort events list by most probable results
 
-        printTenMostProbableResults(eventsList);
+        printMostProbableResults(eventsList);
+
+        System.out.println("How many events do you want to print? ");
+        printMostProbableResults(eventsList, scanner.nextInt());
 
     }
 
-    static void printTenMostProbableResults(ArrayList<Event> EventsList) {
+    static void printMostProbableResults(ArrayList<Event> EventsList) {
         for (int i = 0; i < numberOfEventsToPrint; i++)
-            System.out.println(EventsList.get(i).toString());       // print 10 most probable results
+            System.out.println(EventsList.get(i).toString());           // print 10 most probable results
+    }
+
+    static void printMostProbableResults(ArrayList<Event> EventsList, int NumberOfEventsToPrint) {
+        if (NumberOfEventsToPrint > 0 && NumberOfEventsToPrint <= EventsList.size()) {
+            for (int i = 0; i < NumberOfEventsToPrint; i++)
+                System.out.println(EventsList.get(i).toString());       // print NumberOfEventsToPrint most probable results
+        }
+        else
+            System.out.println("You typed wrong value!");
     }
 }
